81e7ec3cd9928253e416e1db3f117dae743b71ae
hyeongmin90 <nsby8882@naver.com>
2025-07-28 13:56:23 +0900

    remove unused import

--- src/main/java/aivle/project/operation/service/LoginService.java
+++ src/main/java/aivle/project/operation/service/LoginService.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
