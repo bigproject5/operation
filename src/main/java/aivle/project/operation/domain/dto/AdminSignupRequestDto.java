c0731bd3e2c09095d6bb1153568559498b126d50
hyeongmin <nsby8882@naver.com>
2025-08-21 12:39:23 +0900

    회원가입폼 규칙 추가

--- src/main/java/aivle/project/operation/domain/dto/AdminSignupRequestDto.java
+++ src/main/java/aivle/project/operation/domain/dto/AdminSignupRequestDto.java
@@ -1,12 +1,12 @@
 package aivle.project.operation.domain.dto;
 
 import aivle.project.operation.domain.Admin;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Getter
 @Setter
@@ -14,31 +14,48 @@ public class AdminSignupRequestDto {
 
     @NotBlank(message = "Login ID must not be empty.")
     @Size(min = 8, max = 20, message = "Login ID must be between 8 and 20 characters.")
+    @Pattern(
+            regexp = "^[A-Za-z0-9]{8,20}$",
+            message = "Login ID must contain only letters and numbers."
+    )
     private String loginId;
 
     @NotBlank(message = "Password must not be empty.")
+    @Size(min = 8, message = "Password must be over 8 characters.")
     @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,32}$",
-            message = "Password must be 8–32 characters long and include at least one number and one special character."
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\\-_=+{};:,<.>]).{8,}$",
+            message = "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character."
     )
     private String password;
 
-    @NotBlank(message = "name must not be empty.")
+    @NotBlank(message = "Name must not be empty.")
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters.")
+    @Pattern(
+            regexp = "^[가-힣a-zA-Z\\s]{2,20}$",
+            message = "Name must contain only Korean characters, English letters, or spaces."
+    )
     private String name;
 
+    @NotBlank(message = "Email must not be empty.")
+    @Email(message = "Please provide a valid email address.")
     private String email;
 
+    @NotBlank(message = "Phone number must not be empty.")
+    @Pattern(
+            regexp = "^\\d+$",
+            message = "Phone number must contain only numbers."
+    )
     private String phoneNumber;
 
     @NotBlank(message = "Employee number must not be empty.")
     private String employeeNumber;
 
+    @NotBlank(message = "Address must not be empty.")
     private String address;
 
-    @NotBlank(message = "admin code must not be empty.")
+    @NotBlank(message = "Admin code must not be empty.")
     private String adminCode;
 
-//    @NotBlank
     private String reCaptchaToken;
 
     public Admin toEntity(String encodedPassword) {
